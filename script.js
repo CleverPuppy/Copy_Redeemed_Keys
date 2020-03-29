f5163e4c31cec4343089275e5f03ab9a2f88f248
丁一 <dingding1227@hotmail.com>
2020-03-29 23:37:05 +0800

    update UI

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Copy redeemed key
-// @version      0.1
+// @version      0.7
 // @description  A Tampermonkey script for copying redeemed keys of Humble Bundle.com
 // @author       行者丁
 // @namespace    https://github.com/CleverPuppy/Copy_Redeemed_Keys
 // @supportURL   https://github.com/CleverPuppy/Copy_Redeemed_Keys/issues
-// @version      0.1
+// @version      1.0
 // @updateURL    https://raw.githubusercontent.com/CleverPuppy/Copy_Redeemed_Keys/master/script.js
 // @downloadURL  https://raw.githubusercontent.com/CleverPuppy/Copy_Redeemed_Keys/master/script.js
 // @icon         https://humblebundle-a.akamaihd.net/static/hashed/46cf2ed85a0641bfdc052121786440c70da77d75.png
@@ -37,10 +37,15 @@
             var item_key = el.getElementsByClassName('keyfield-value')[0].innerText;
             key_list.push({ 'game_name': item_name, "key": item_key });
         };
-        // console.log(key_list_to_string(key_list));
-        insertHTML(key_list_to_string(key_list));
+        return key_list_to_string(key_list);
     };
-    const insertHTML = function (contentVal){
+    const insertHTML = function (){
+        if(document.getElementsByClassName('key-list').length == 0){
+            setTimeout(function(){
+                insertHTML();
+            },500);
+            return;
+        };
         const base = document.getElementsByClassName('key-list')[0].parentElement;
         const insertElem = document.createElement("div");
         const hintElem = document.createElement('span');
@@ -48,12 +53,13 @@
         insertElem.appendChild(hintElem);
         const contentElem = document.createElement('textarea');
         contentElem.setAttribute('style','width: 770px; height: 260px');
-        contentElem.textContent = contentVal;
+        contentElem.textContent = "点击下方按键进行操作：\r [复制到剪贴板] ： 复制当前所有key的文字信息到剪贴板，并展示到这个区域(如果有未刮开的key,则会显示'Reveal xxx'的字样)  \r [生成key的截图] ： 将会生成这个网页当前key的截图，有需要可以自行复制";
         insertElem.appendChild(contentElem);
 
         const buttonElem = document.createElement('button');
         buttonElem.textContent = "复制到剪贴板";
         buttonElem.onclick = function(){
+            contentElem.textContent = get_redeemed_keys();
             contentElem.select();
             document.execCommand('Copy');
             alert('已复制到剪贴板');
@@ -70,11 +76,12 @@
             const keyListElem = document.getElementsByClassName('key-list')[0];
             html2canvas(keyListElem).then(canvas => {
                 imageElem.src = canvas.toDataURL();
+                imageElem.setAttribute('style','border:5px solid black');
             });
         };
         insertElem.appendChild(imageButtonElem);
 
         base.insertBefore(insertElem,base.firstChild);
     };
-    get_redeemed_keys();
+    insertHTML();
 })();
